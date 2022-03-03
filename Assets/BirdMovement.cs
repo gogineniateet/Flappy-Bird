5a5385cc4fc82000805f859ec7fe406bf0b7ddab
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-03-03 16:14:07 +0530

    Bird Flap done

---
+++ Assets/BirdMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float birdSpeed;
+
+    private void Awake()
+    {
+        gameObject.AddComponent<Rigidbody2D>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // bird movement will start after left click mouse button
+            BirdFlapAndJump();
+        }
+
+
+    }
+
+    private void BirdFlapAndJump()
+    {
+        rb.velocity = Vector2.zero;
+        rb.velocity = new Vector2(rb.velocity.x, birdSpeed);
+    }
+}
+
+
