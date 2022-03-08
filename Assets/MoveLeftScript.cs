dd5ef4d0216888d90b01c77be49e4f0561ce9f6b
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-03-08 15:53:11 +0530

    Spawning pipe and moving to left side

---
+++ Assets/MoveLeftScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftScript : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x  - speed * Time.deltaTime, transform.position.y);
+        if (transform.position.x < -4f)
+        {
+            Destroy(gameObject);
+         
+        }
+    }
+}
---
+++ Assets/PipeSpawner.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    public float minY, maxY;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("PipeSpawnner", 2f, 2f);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void PipeSpawnner()
+    {
+        GameObject newPipe = Instantiate(pipePrefab);
+        newPipe.transform.position = new Vector2(transform.position.x, UnityEngine.Random.Range(minY, maxY));       
+    }
+
+
+}
